263c9de0f4741117b4ab2d75b35989607506b529
Jordan Blakey <jordan@jordanblakey.com>
2017-09-29 03:02:14 -0500

    init

---
+++ types.js
@@ -0,0 +1,11 @@
+var myName = "Derek";
+var myAge = 41;
+var canVote = true;
+var anything = "dog";
+inferred = 2;
+document.getElementById('ts').innerHTML = 'Hi, my name is ' + myName + '.';
+document.write('myName is a ' + typeof (myName) + '<br>');
+document.write('myAge is a ' + typeof (myAge) + '<br>');
+document.write('canVote is a ' + typeof (canVote) + '<br>');
+document.write('anything is a ' + typeof (anything) + '<br>');
+document.write('inferred is a ' + typeof (inferred) + '<br>');
---
+++ types.ts
@@ -0,0 +1,13 @@
+var myName: string = "Derek";
+var myAge: number = 41;
+var canVote: boolean = true;
+var anything: any = "dog";
+inferred = 2;
+
+document.getElementById('ts').innerHTML = 'Hi, my name is ' + myName + '.';
+
+document.write('myName is a ' + typeof(myName) + '<br>');
+document.write('myAge is a ' + typeof(myAge) + '<br>');
+document.write('canVote is a ' + typeof(canVote) + '<br>');
+document.write('anything is a ' + typeof(anything) + '<br>');
+document.write('inferred is a ' + typeof(inferred) + '<br>');
